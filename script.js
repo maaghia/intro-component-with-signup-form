066491fdc9958bfe6506fc980e3beac93a7c96c7
maaghia <toubalmaria23@gmail.com>
2022-07-28 12:11:27 +0100

    js-fail

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const firstName = document.querySelector("#first-name");
-const lastName = document.querySelector("#last-name");
-const email = document.querySelector("email");
-const msgEmail = document.querySelector("#email-error");
-const iconEmail = document.querySelector("#email-icon");
-const passeword = document.querySelector("passeword");
-const button = document.querySelector("submit");
-const iconFirstName = document.querySelector("#first-name");
-const iconLastName = document.querySelector("#last-name");
-const iconPassword = document.querySelector("#icon-passeword-error");
-  //invalid email
+  //data collected
+  const form = document.querySelector('#form')
+  const firstName = document.querySelector("#first-name");
+  const lastName = document.querySelector("#last-name");
+  const email = document.querySelector("email");
+  const passeword = document.querySelector("passeword");
+  const button = document.querySelector("submit");
+  //error messages
+  const msgFirstName = document.querySelector("#fname-error");
+  const msgLastName = document.querySelector("#lname-error");
+  const msgEmail = document.querySelector("#email-error");
+  const msgPassword = document.querySelector("#password-error");
+  //error icons
+  const iconFirstName = document.querySelector("#icon-firstname-error");
+  const iconLastName = document.querySelector("#icon-lasstname-error");
+  const iconEmail = document.querySelector("#icon-email-error");
+  const iconPassword = document.querySelector("#icon-password-error");
+
+  //invalid email function
 function ValidateEmail(mail) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (mail.match(mailformat)) {
@@ -30,11 +38,60 @@ function ValidateEmail(mail) {
         msgEmail.style.visibility = "hidden";
     }
   }
-  //empty input
-  function IsEmpty(data) {
+
+  //empty input function
+  function IsEmpty(data, icon, msg) {
   if (document.forms['frm'].data.value === "") {
     return false;
   }
     return true;
 }
-
+  //verify first name not empty
+const submitFN=() => {
+  const userFN = firstName.value; 
+  const fnIcon = iconFirstName.value;
+  const fnMsg = msgFirstName.value;
+  if (!IsEmpty(userFN,fnIcon,fnMsg)){
+      iconFirstName.style.visibility = "visible";
+      msgFirstName.style.visibility = "visible";
+  }
+  else{
+    iconFirstName.style.visibility = "hidden";
+    msgFirstName.style.visibility = "hidden";
+  }
+}
+  //verify last name not empty
+  const submitLN=() => {
+    const userLN = firstName.value; 
+    const lnIcon = iconFirstName.value;
+    const lnMsg = msgFirstName.value;
+    if (!IsEmpty(userLN,lnIcon,lnMsg)){
+        iconLastName.style.visibility = "visible";
+        msgLastName.style.visibility = "visible";
+    }
+    else{
+      iconLastName.style.visibility = "hidden";
+      msgLastName.style.visibility = "hidden";
+    }
+  }
+    //verify password not empty
+    const submitPassword=() => {
+      const userPassword = firstName.value; 
+      const passwordIcon = iconFirstName.value;
+      const passwordMsg = msgFirstName.value;
+      if (!IsEmpty(userPassword,passwordIcon,passwordMsg)){
+          iconPassword.style.visibility = "visible";
+          msgPassword.style.visibility = "visible";
+      }
+      else{
+        iconPassword.style.visibility = "hidden";
+        msgPassword.style.visibility = "hidden";
+      }
+    }
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      submitEmail();
+      submitFN();
+      submitLN();
+      submitPassword();
+    });
